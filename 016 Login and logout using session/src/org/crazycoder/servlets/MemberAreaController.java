2e1eadbeff2a9f29c8016881d9705ed8b2f2f004
TryMe <TryMe@DESKTOP-ETLRID9>
2020-12-16 14:51:36 +0530

    Login and Logout using sessions

---
+++ 016 Login and logout using session/src/org/crazycoder/servlets/MemberAreaController.java
@@ -0,0 +1,39 @@
+package org.crazycoder.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/MemberAreaController")
+public class MemberAreaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public MemberAreaController() {
+
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		switch (action) {
+		case "destroy": 
+			request.getSession().invalidate();	
+			response.sendRedirect("login.jsp");
+			 break;
+		default:
+			break;
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
